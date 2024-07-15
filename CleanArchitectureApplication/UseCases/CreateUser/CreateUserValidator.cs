f08d7bcaa7c6989e001d4b985234fdf9b874297d
carlosedu757 <74271104+carlosedu757@users.noreply.github.com>
2024-07-15 12:24:19 -0300

    Implementação da camada de aplicação

---
+++ CleanArchitectureApplication/UseCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser;
+
+public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>
+{
+    // AQUI NESSE CASO EU PODERIA USAR DATAANNOTATIONS NA ENTIDADE
+    public CreateUserValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().MaximumLength(50).EmailAddress();
+        RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+    }
+}
